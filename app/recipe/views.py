11a11fb70b74ecdb18a84541218cc804be8598ba
ShyGY0806 <72110090+GaryYangRISA@users.noreply.github.com>
2023-09-16 23:53:22 -0700

    Tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -48,3 +51,4 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
